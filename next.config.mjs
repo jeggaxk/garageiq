70e3f60a5993e4fb4b59df18e196c7dd7d2a648e
jeggaxk <jeggaxk@gmail.com>
2026-04-02 19:52:51 +0100

    Fix Next.js config key for external packages

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverComponentsExternalPackages: ['twilio'],
-  },
+  serverExternalPackages: ['twilio'],
 }
 
 export default nextConfig
